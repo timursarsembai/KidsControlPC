796bc6a792f278abf005c38d3cf98a8889dd7833
Timur Sarsembai <118261557+timursarsembai@users.noreply.github.com>
2026-05-25 11:59:22 +0500

    fix: force nsis language dialog

--- agent/build_installer.js
+++ agent/build_installer.js
@@ -65,11 +65,6 @@ OutFile "KidsControlAgent_Setup.exe"
 InstallDir "$PROGRAMFILES64\\KidsControlAgent"
 RequestExecutionLevel admin
 
-!define MUI_LANGDLL_REGISTRY_ROOT "HKCU"
-!define MUI_LANGDLL_REGISTRY_KEY "Software\\KidsControlPCAgent"
-!define MUI_LANGDLL_REGISTRY_VALUENAME "Installer Language"
-!define MUI_LANGDLL_ALWAYSSHOW
-
 !insertmacro MUI_PAGE_WELCOME
 !insertmacro MUI_PAGE_DIRECTORY
 !insertmacro MUI_PAGE_INSTFILES
